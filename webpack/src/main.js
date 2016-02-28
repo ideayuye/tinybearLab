a437096acda06f6d6e14b6c4d8d472a1aab0e203
tinybear <576135266@qq.com>
2016-02-28 22:25:12 +0800

    笔记本修改

--- webpack/src/main.js
+++ webpack/src/main.js
@@ -2,16 +2,16 @@
 
 var a = require('./js/a');
 var common = require('./js/common');
-require('./css/index.css');
+// require('./css/index.css');
 
 common();
 a();
-console.log('hello world');
-document.getElementById("container").innerHTML = "<p>hello world</p>";
+// console.log('hello world');
+// document.getElementById("container").innerHTML = "<p>hello world</p>";
 
 
- var template = require("./template/tp.html");
- var data={say_hello:"it is handlebars"};
- var html = template(data);
- document.getElementById('tmpl_container').innerHTML = html;
+//  var template = require("./template/tp.html");
+//  var data={say_hello:"it is handlebars"};
+//  var html = template(data);
+//  document.getElementById('tmpl_container').innerHTML = html;
 
--- webpack/webpack-config.js
+++ webpack/webpack-config.js
@@ -6,8 +6,8 @@ var nodemodulesPath = path.resolve(__dirname,'node_modules');
 var pathToHandlebars = path.resolve(nodemodulesPath, 'handlebars/dist/handlebars.min.js');
 
 var config = {
-	entry:{app:path.resolve(__dirname,'src/main.js'),vendor: ["./src/js/common"]},
-	// entry:{m1:path.resolve(__dirname,'src/main.js'),
+	// entry:{app:path.resolve(__dirname,'src/main.js'),vendor: ["./src/js/common"]},
+	entry:{m1:path.resolve(__dirname,'src/main.js')},
 	// 	m2:path.resolve(__dirname,'src/main1.js')},
 	resolve:{
 		extentions:["","js"],
@@ -18,38 +18,38 @@ var config = {
 	output:{
 		path:buildPath,
 		filename:"app.js"
-	},
-	module:{
-		loaders:[{
-			test:/\.css$/,
-			loader:'style!css',
-			exclude:nodemodulesPath
-		},
-		{ test:/\.(png|woff|svg|ttf|eot)$/,loader:'url-loader?limit=10000'},
-		{ test: /\.jpg$/, loader: "file-loader?name=[sha512:hash:base64:7].[ext]" },
-		{ test: /\.html$/, loader: "handlebars-loader" }
-		]
-	},
-	plugins:[
-		// new webpack.optimize.UglifyJsPlugin({
-		//     compress: {
-		//        //supresses warnings, usually from module minification
-		//        warnings: false
-		//     }
-		// }),
-		new webpack.optimize.CommonsChunkPlugin({
-			name: "vendor",
-			// (the commons chunk name)
-			filename: "vendor.js",
-			// (the filename of the commons chunk)
+	}
+	// module:{
+	// 	loaders:[{
+	// 		test:/\.css$/,
+	// 		loader:'style!css',
+	// 		exclude:nodemodulesPath
+	// 	},
+	// 	{ test:/\.(png|woff|svg|ttf|eot)$/,loader:'url-loader?limit=10000'},
+	// 	{ test: /\.jpg$/, loader: "file-loader?name=[sha512:hash:base64:7].[ext]" },
+	// 	{ test: /\.html$/, loader: "handlebars-loader" }
+	// 	]
+	// },
+	// plugins:[
+	// 	// new webpack.optimize.UglifyJsPlugin({
+	// 	//     compress: {
+	// 	//        //supresses warnings, usually from module minification
+	// 	//        warnings: false
+	// 	//     }
+	// 	// }),
+	// 	new webpack.optimize.CommonsChunkPlugin({
+	// 		name: "vendor",
+	// 		// (the commons chunk name)
+	// 		filename: "vendor.js",
+	// 		// (the filename of the commons chunk)
 
-			// minChunks: 3,
-			// (Modules must be shared between 3 entries)
+	// 		// minChunks: 3,
+	// 		// (Modules must be shared between 3 entries)
 
-			// chunks: ["pageA", "pageB"],
-			// (Only use these entries)
-		})
-	]
+	// 		// chunks: ["pageA", "pageB"],
+	// 		// (Only use these entries)
+	// 	})
+	// ]
 	
 }
 
