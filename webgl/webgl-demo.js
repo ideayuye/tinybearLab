aff9758aca13d05b70bf017e56bc4217c87652ca
ideayuye <576135266@qq.com>
2016-04-21 14:10:03 +0800

    render color

--- webgl/webgl-demo.js
+++ webgl/webgl-demo.js
@@ -5,10 +5,12 @@
 
 var canvas;
 var gl;
-var  squareVerticesBuffer;
+var squareVerticesBuffer;
+var squareVerticesColorBuffer;
 var mvMatrix;
 var shaderProgram;
 var vertexPositionAttribute;
+var vertexColorAttribute;
 var perspectiveMatrix;
 
 //
@@ -111,8 +113,11 @@ function initShaders(){
 
     gl.useProgram(shaderProgram);
 
-    var vertexPositionAttribute = gl.getAttribLocation(shaderProgram,"aVertexPosition");
+    vertexPositionAttribute = gl.getAttribLocation(shaderProgram,"aVertexPosition");
     gl.enableVertexAttribArray(vertexPositionAttribute);
+
+    vertexColorAttribute = gl.getAttribLocation(shaderProgram,"aVertexColor");
+    gl.enableVertexAttribArray(vertexColorAttribute);
 
 }
 
@@ -130,6 +135,17 @@ function initBufers(){
 
     gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(vertices),gl.STATIC_DRAW);
 
+    var colors = [
+        1.0,  1.0,  1.0,  1.0,    // white
+        1.0,  0.0,  0.0,  1.0,    // red
+        0.0,  1.0,  0.0,  1.0,    // green
+        0.0,  0.0,  1.0,  1.0     // blue
+    ];
+
+    squareVerticesColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER,squareVerticesColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(colors),gl.STATIC_DRAW);
+
 }
 
 function drawScene(){
@@ -142,11 +158,14 @@ function drawScene(){
 
     gl.bindBuffer(gl.ARRAY_BUFFER,squareVerticesBuffer);
     gl.vertexAttribPointer(vertexPositionAttribute,3,gl.FLOAT,false,0,0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER,squareVerticesColorBuffer);
+    gl.vertexAttribPointer(vertexColorAttribute,4,gl.FLOAT,false,0,0);
+
     setMatrixUniforms();
     gl.drawArrays(gl.TRIANGLE_STRIP,0,4);
 
     window.requestAnimationFrame(drawScene);
-
 }
 
 function loadIdentity() {
