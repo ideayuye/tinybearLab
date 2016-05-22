b444630fd3957d936e2ebffc56a1964a225b2a95
tinybear <576135266@qq.com>
2016-05-22 22:41:27 +0800

    buer home

---
+++ buerHome/html/gulpfile.js
@@ -0,0 +1,37 @@
+
+var gulp = require('gulp');
+
+var imagemin = require('gulp-imagemin');
+var cssnano = require('gulp-cssnano');
+var uglify = require('gulp-uglify');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+
+gulp.task('image-min', () =>
+    gulp.src('images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
+
+gulp.task('css', function() {
+    return gulp.src('./css/*.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('js',function(){
+    return gulp.src('./js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('html', function () {
+    return gulp.src('index.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', cssnano({zIndex:false}))) 
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build',['image-min','html']);
+
