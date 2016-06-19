128f53117c75449500781b6f7a567c674d883fea
ideayuye <576135266@qq.com>
2016-06-19 18:02:31 +0800

    http test

--- test/httpTest.js
+++ test/httpTest.js
@@ -1,13 +1,30 @@
 
-var http = require('http');
+//var http = require('http');
+//
+//var server =  http.createServer((req,res)=>{
+//
+//    console.log('sever start');
+//    res.writeHead(200,{"Content-Type": "text/plain"});
+//    res.write("hello");
+//    res.end();
+//
+//
+//}).listen(1111);
+//
+//console.log('start 1111');
 
-var server =  http.createServer((req,res)=>{
-    
-    console.log('sever start'); 
-    res.writeHead(200,{"Content-Type": "text/plain"});
-    res.write("hello");
-    res.end();
-    
-}).listen(1111);
+var express = require('express');
+var app = express();
+
+app.use('/',(req,res)=>{
+    console.log(res.getHeader('Set-Cookie'));
+    res.end('cookie parse');
+});
+
+app.listen('1111');
+console.log('start 1111');
+
+
+
 
 
