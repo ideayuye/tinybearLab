53210c89251c73b10b57fecf3aecaa929e049720
ideayuye <576135266@qq.com>
2016-07-04 19:55:35 +0800

    redux start

---
+++ react/HI.js
@@ -0,0 +1,11 @@
+
+var React = require('react');
+
+var Hello = React.createClass({
+	render:()=>{
+		return (<h1>Hello xxxxx</h1>);
+	}
+});
+
+module.exports = Hello;
+
--- react/hello.js
+++ react/hello.js
@@ -1,13 +1,5 @@
 
-// var t = "hello";
 
-// var xo = ()=>{
-// 	console.log('xxx:'+t);
-// }
-
-// xo();
-
-// console.log("start");
 var React = require('react')
 var reactDom = require("react-dom");
 var HI = require('./HI');
