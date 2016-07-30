d57c38fb4e86f81166a8772421cbc904e8d0a1db
ideayuye <576135266@qq.com>
2016-07-30 18:22:15 +0800

    draw mark

--- web-ruler/src/lengthMark.js
+++ web-ruler/src/lengthMark.js
@@ -4,26 +4,60 @@ var LengthMark = function(ctx){
     this.id = "lm_" + (new Date()).valueOf();
     this.p1={x:0,y:0,isOK:0};
     this.p2={x:0,y:0,isOK:0};
+    this.dir = "";
     this.setP1=function(x,y){
-        this.p1.x = x;
-        this.p1.y = y;
+        this.p1.x = x+0.5;
+        this.p1.y = y+0.5;
         this.p1.isOK = 1;
     };
     this.setP2=function(x,y){
         var dir = this.judgeDir(x,y);
+        this.dir = dir;
         if(dir == "v"){
             this.p2.x = this.p1.x;
-            this.p2.y = y;
+            this.p2.y = y+0.5;
         }
         if(dir == "h"){
-            this.p2.x = x;
+            this.p2.x = x+0.5;
             this.p2.y = this.p1.y;
         }
+        this.calMarkPos();
         this.p2.isOK = 1;
     };
     this.step = 0;
     this.isEnd = function(){
         return this.step == 3? true:false;
+    };
+    this.dx = function(){
+        var dx = 0;
+        if(this.p1.isOK && this.p2.isOK){
+            dx = this.p1.x - this.p2.x;
+            dx = Math.abs(dx);
+        }
+        return dx;
+    };
+    this.dy = function(){
+        var dy = 0;
+        if(this.p1.isOK && this.p2.isOK){
+            dy = this.p1.y - this.p2.y;
+            dy = Math.abs(dy);
+        }
+        return dy;
+    };
+    this.markPos = {x:0,y:0};
+    //计算长度标记的位置
+    this.calMarkPos = function(){
+        var dir = this.dir;
+        var x,y;
+        if(dir == "v"){
+            x = this.p1.x;
+            y = Math.round((this.p1.y+this.p2.y)*0.5);
+        }
+        if(dir == "h"){
+            x = Math.round((this.p1.x+this.p2.x)*0.5);
+            y = this.p1.y;
+        }
+        this.markPos = {x:x,y:y};
     };
     //判断方向
     this.judgeDir = function (x,y) {
@@ -37,7 +71,7 @@ var LengthMark = function(ctx){
             else
                 return "h";
         }
-    }
+    };
     this.process=function(data){
         switch(data.mouseType){
             case "mousedown":
@@ -63,24 +97,76 @@ var LengthMark = function(ctx){
     this.draw=function(){
         if(this.step < 2)
             return;
+        this.ctx.stokeStyle = '#FE1616';
         this.ctx.beginPath();
+        this.ctx.lineWidth = 1;
         this.ctx.moveTo(this.p1.x,this.p1.y);
         this.ctx.lineTo(this.p2.x,this.p2.y);
         this.ctx.stroke();
         this.ctx.closePath();
+        this.drawMark();
+        this.drawNode();
+        if(this.step == 2){
+            this.drawDottedLine();
+        }
     };
     //绘制节点
     this.drawNode=function(){
-        this.ctx.fillStyle = "#07a";
-        if(this.p1.isOK){
-            this.ctx.fillRect(this.p1.x-2, this.p1.y-2, 4, 4);
+        this.ctx.beginPath();
+        if(this.dir == "v"){
+            this.ctx.moveTo(this.p1.x-5,this.p1.y);
+            this.ctx.lineTo(this.p1.x+5,this.p1.y);
+            this.ctx.moveTo(this.p2.x-5,this.p2.y);
+            this.ctx.lineTo(this.p2.x+5,this.p2.y);
         }
-        if(this.p2.isOK){
-            this.ctx.fillRect(this.p2.x-2, this.p2.y-2, 4, 4);
+        if(this.dir == "h"){
+            this.ctx.moveTo(this.p1.x,this.p1.y-5);
+            this.ctx.lineTo(this.p1.x,this.p1.y+5);
+            this.ctx.moveTo(this.p2.x,this.p2.y-5);
+            this.ctx.lineTo(this.p2.x,this.p2.y+5);
         }
+        this.ctx.stroke();
+        this.ctx.closePath();
     };
     //绘制标注
-    
+    this.drawMark = function(){
+        var length = "";
+        if(this.dir == "v"){
+            length = this.dy();
+        }
+        if(this.dir == "h"){
+            length = this.dx();
+        }
+        this.ctx.font = "16px arial";
+        this.ctx.fillStyle = '#FE1616';
+        var mtxt = this.ctx.measureText(length);
+        if(this.dir == "v")
+            this.ctx.fillText(length, this.markPos.x-mtxt.width*0.5, this.markPos.y);
+        if(this.dir == "h")
+            this.ctx.fillText(length, this.markPos.x, this.markPos.y);
+    };
+    //绘制虚线
+    this.drawDottedLine = function(){
+        this.ctx.save();
+        this.ctx.beginPath();
+        this.ctx.setLineDash([5]);
+        if(this.dir == "v"){
+            this.ctx.moveTo(this.p1.x-1000,this.p1.y);
+            this.ctx.lineTo(this.p1.x+1000,this.p1.y);
+            this.ctx.moveTo(this.p2.x-1000,this.p2.y);
+            this.ctx.lineTo(this.p2.x+1000,this.p2.y);
+        }
+        if(this.dir == "h"){
+            this.ctx.moveTo(this.p1.x,this.p1.y-1000);
+            this.ctx.lineTo(this.p1.x,this.p1.y+1000);
+            this.ctx.moveTo(this.p2.x,this.p2.y-1000);
+            this.ctx.lineTo(this.p2.x,this.p2.y+1000);
+        }
+        this.ctx.stroke();
+        this.ctx.closePath();
+        this.ctx.restore();
+    };
 };
 
 module.exports = LengthMark; 
+
