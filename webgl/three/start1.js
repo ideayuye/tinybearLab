a0b22578729f9be88394a50537dc2e554b57866a
ideayuye <tinybear@zhujinxiongdeMacBook-Pro.local>
2016-09-11 22:49:31 +0800

    lambert material

--- webgl/three/start1.js
+++ webgl/three/start1.js
@@ -2,18 +2,30 @@
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,10);
-camera.position.set(2,0,5);
+camera.position.set(0,0,5);
 
 var render = new THREE.WebGLRenderer({
     canvas:document.getElementById('test')
 });
+render.setPixelRatio(window.devicePixelRatio);
 
 render.setSize(window.innerWidth,window.innerHeight);
 render.setClearColor(0x000000);
 
 var geo = new THREE.BoxGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial({color:0xff0000,wireframe :true});
+/*var material = new THREE.MeshBasicMaterial({
+    color:0xff6600,
+    // wireframe :true
+});*/
+var material = new THREE.MeshLambertMaterial({
+    color:"#2194ce",
+    emissive:"#000000"
+    // wireframe:true
+});
 var cube = new THREE.Mesh(geo,material);
+
+cube.rotation.y = 45 % (Math.PI * 2);
+cube.rotation.x = 45 % (Math.PI * 2);
 scene.add(cube);
 
 var lineMaterial = new THREE.LineBasicMaterial({color: 0x0000ff});
@@ -41,6 +53,19 @@ var line2 = new THREE.Line(geoLine2,lineMaterialG);
 scene.add(line);
 scene.add(line1);
 scene.add(line2);
+
+//light
+var ambientLight = new THREE.AmbientLight(0x000000);
+scene.add(ambientLight);
+var light = new THREE.PointLight(0xffffff,1,0);
+light.position.set(-5,2,3);
+var light1 = new THREE.PointLight(0xffffff,1,0);
+light1.position.set(5,2,3);
+var light2 = new THREE.PointLight(0xffffff,1,0);
+light2.position.set(0,-5,3);
+scene.add(light);
+scene.add(light1);
+scene.add(light2);
 
 render.render(scene,camera);
 
