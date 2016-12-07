57a54f902568afe16b8a52b0abae2aeb1499fb77
ideayuye <576135266@qq.com>
2016-12-07 22:47:37 +0800

    start fine

--- karma/test/test.js
+++ karma/test/test.js
@@ -1,8 +1,9 @@
 
 
+// var expect = require('chai').expect;
 
-describe('t1',()=>{
-    it('case1',()=>{
+describe('t1',function(){
+    it('case1',function(){
         expect(1+2).to.equal(3);
     })
 })
