7628fcf8f96b94e46d1674c67c4f145f0ae4ecdd
ideayuye <576135266@qq.com>
2016-12-13 22:41:10 +0800

    regexp

--- test/regexp.js
+++ test/regexp.js
@@ -5,11 +5,20 @@ var reg = new RegExp('opacity=(\\d+)');
 console.log(s1.match(reg));
 
 var s2 = "<div class='blx'><b>hello</b><i>nii</i></div>";
-var reg2 = /<(\/?)(\w+)([^>]*?)>/g;
+var reg2 = /<(?:\/?)(?:\w+)([^>]*?)>/;
 var match ;
-while(match = reg2.exec(s2) !== null){
+/*while((match = reg2.exec(s2)) !== null){
     console.log(match);
+}*/
+console.log(s2.match(reg2));
+
+function upper(all,letter){
+    return letter.toUpperCase();
 }
-// console.log(s2.match(reg2));
+
+var s3 = "border-bottom-width";
+var reg3 = /-(\w)/g;
+var res3 = s3.replace(reg3,upper);
+console.log(res3);
 
 
