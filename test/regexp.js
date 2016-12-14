af32dcfa1fffc4fd560396a72c19cfcd7f901c80
ideayuye <576135266@qq.com>
2016-12-14 17:21:31 +0800

    check tag closed

--- test/regexp.js
+++ test/regexp.js
@@ -1,24 +1,25 @@
+var assert = require('assert');
 
 var s1 = "filter:alpha(opacity=50);";
 
 var reg = new RegExp('opacity=(\\d+)');
-console.log(s1.match(reg));
+// console.log(s1.match(reg));
 
 var s2 = "<div class='blx'><b>hello</b><i>nii</i></div>";
 var reg2 = /<(?:\/?)(?:\w+)([^>]*?)>/;
-var match ;
+var match;
 /*while((match = reg2.exec(s2)) !== null){
     console.log(match);
 }*/
-console.log(s2.match(reg2));
+// console.log(s2.match(reg2));
 
-function upper(all,letter){
+function upper(all, letter) {
     return letter.toUpperCase();
 }
 
 var s3 = "border-bottom-width";
 var reg3 = /-(\w)/g;
-var res3 = s3.replace(reg3,upper);
-console.log(res3);
+var res3 = s3.replace(reg3, upper);
+// console.log(res3);
 
 
