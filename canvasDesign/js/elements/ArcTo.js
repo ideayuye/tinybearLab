ce136c566769b4dff52409c63939c506e8c0f73c
zhujinxiong <jx.zhu@linkwise.cn>
2025-09-03 18:03:37 +0800

    feat: remove some project

--- canvasDesign/js/elements/ArcTo.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by 15050971 on 2016/4/14.
- */
-
-var ArcTo = function(){
-
-};
-
-module.exports = ArcTo;
--- test/import.js
+++
@@ -1,6 +0,0 @@
-
-'use strict'
-/*6.9.1 不支持动态引入*/
-import('./module.js').then(modules=>{
-    console.log(modules);
-})
